110f2c2082eae05e64679e71e1026189379cd40e
Leonardo <lcastro@cleartech.com.br>
2016-03-23 18:22:44 -0300

    Inicio de aprendizado MEAN com delete

--- public/js/controllers/contatosController.js
+++ public/js/controllers/contatosController.js
@@ -1,6 +1,6 @@
 
 angular.module('contact').controller('contatosController',
-	function($scope,$resource) {
+	function($scope,$resource,$routeParams) {
 
 		$scope.mensagem = {texto: ''};
 		$scope.contatos = [];
@@ -8,13 +8,40 @@ angular.module('contact').controller('contatosController',
 		
 		var Contato = $resource('/contatos/:id');
 
+		if($routeParams.contatoId) {
+			Contato.get({id: $routeParams.contatoId},
+				function(contato) {
+					$scope.contato = contato;
+				},
+				function(erro) {
+					$scope.mensagem = {
+						texto: 'Contato não existe. Novo contato.'
+					};
+				}
+				);
+		} else {
+			$scope.contato = {};
+		}
+
+		$scope.salva = function() {
+			$scope.contato.$save()
+			.then(function() {
+				$scope.mensagem = {texto: 'Salvo com sucesso'};
+					// limpa o formulário
+					$scope.contato = new Contato();
+				})
+			.catch(function(erro) {
+				$scope.mensagem = {texto: 'Não foi possível salvar'};
+			});
+		};
+
 		$scope.remove = function(contato) {
 			Contato.delete({id: contato._id},
-			buscaContatos,
+				buscaContatos,
 				function(erro) {
 					$scope.mensagem = {texto: 'Não foi possível remover o contato'};
 				}
-			);
+				);
 		};
 
 		function buscaContatos() {
@@ -25,7 +52,7 @@ angular.module('contact').controller('contatosController',
 				function(erro) {					
 					$scope.mensagem = { texto: 'Não foi possível obter a lista' };
 				}
-			);
+				);
 		}
 		buscaContatos(); 
 	});
